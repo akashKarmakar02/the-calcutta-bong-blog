923222e4c0158a63d074239b8497829c6d0e6d9c
akashKarmakar02 <akash.anan03@gmail.com>
2023-04-28 00:19:53 +0530

    Custom NavBar for Sanity Studio

---
+++ components/StudioNavBar.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import {ArrowUturnLeftIcon} from '@heroicons/react/24/solid'
+
+export default function StudioNavBar(props: any) {
+  return (
+    <div>
+        <div className="flex items-center justify-between p-5">
+            <Link href='/' className="text-[#F7AB0A] flex items-center">
+                <ArrowUturnLeftIcon className="h-6 w-6 text-[#F7AB0A] mr-2" />
+                Go To Website
+            </Link>
+        </div>
+        <>{props.renderDefault(props)}</>
+    </div>
+  )
+}
