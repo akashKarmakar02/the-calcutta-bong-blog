20f6dd8b48b9856154f00f7132f6e34897f1ef5e
akashKarmakar02 <akash.anan03@gmail.com>
2023-04-28 20:09:24 +0530

    fixed the grid bug

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -13,7 +13,7 @@ function BlogList({posts}: Props) {
     <div>
         <hr className="border-[#F7AB0A] mb-10"/>
 
-        <div className="grid grid-cols-1 md:grid-col-2 px-10 gap-10 gap-y-16 pb-24">
+        <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
             {/* Posts */}
             {posts.map((post) => {
                 return (
