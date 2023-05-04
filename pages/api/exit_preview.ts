5dd75e971ebc60afe85455ec04ab30caf0486269
akashKarmakar02 <akash.anan03@gmail.com>
2023-05-04 23:08:20 +0530

    deleting preview api

--- pages/api/exit_preview.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default function exit(req: NextApiRequest, res: NextApiResponse) {
-    res.clearPreviewData();
-    console.log(req.previewData)
-    res.writeHead(307, {Location: '/'});
-    res.end()
-}
